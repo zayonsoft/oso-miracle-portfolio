18fc9aeb3331bf0d63c6a33f5b9ff4400d5cda21
zayonsoft <favourlosotu@gmail.com>
2025-08-05 07:16:48 +0100

    Added The dotted design beneath the Profile Image and adjusted the skills image

--- src/pages/home/components/about/ProfileImage.jsx
+++ src/pages/home/components/about/ProfileImage.jsx
@@ -1,8 +1,11 @@
 import ProfileImagePic from "../../styles/about/profile_image.png";
 import ProfileBottom from "./ProfileBottom";
+import ProfilePicBlueDesign from "../../styles/about/about_under_image_line.png";
 export default function ProfileImage() {
   return (
     <section className="relative col-span-2 max-[661px]:pb-[110px]">
+      {/* Div That Overlays everything and prevents the images from being dragable individually */}
+      <div className="absolute left-0 right-0 top-0 bottom-0 z-50 bg-transparent"></div>
       {/* background box */}
       <div className="pt-24">
         {/* Upper Box */}
@@ -18,6 +21,9 @@ export default function ProfileImage() {
         </div>
         <div className="relative w-52 m-auto">
           <ProfileBottom />
+          <div className="absolute top-2.5">
+            <img src={ProfilePicBlueDesign} alt="" />
+          </div>
         </div>
       </div>
     </section>
--- src/pages/home/components/skillchart/SkillSection.jsx
+++ src/pages/home/components/skillchart/SkillSection.jsx
@@ -1,9 +1,10 @@
 import SkillImage from "../../styles/skill/skill_image.png";
+import "../../styles/skill/skills.css";
 export default function SkillSection() {
   return (
     <section className="grid content-center min-h-screen w-full bg-white">
       <img
-        className="max-w-[500px] w-20% block m-auto  max-[601px]:max-w-[380px] skills-bg"
+        className="max-w-[600px] object-center object-contain w-20% block m-auto  max-[601px]:max-w-[380px] skills-bg"
         src={SkillImage}
         alt=""
       />
